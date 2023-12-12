a361f0455fdf1955fa641d28838e1b7272b31028
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2023-12-12 23:20:00 +0100

    Created a "unsafe" password manager implementation

--- SecureApi.Web/Services/Cryptography/PasswordManagerV1.cs
+++ SecureApi.Web/Services/Cryptography/PasswordManagerV1.cs
@@ -1,19 +1,40 @@
-﻿using SecureApi.Web.Models;
+﻿using Microsoft.Extensions.Options;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace SecureApi.Web.Services.Cryptography;
 
 public class PasswordManagerV1
     : PasswordManagerBase
 {
+    private readonly PasswordManagerV1Options _options;
+
     protected override int Version => 1;
+
+    public PasswordManagerV1(IOptions<PasswordManagerV1Options> options)
+    {
+        _options = options.Value;
+    }
 
     protected override byte[] GenerateSalt()
     {
-        throw new NotImplementedException();
+        return RandomNumberGenerator.GetBytes(_options.SaltSize);
     }
 
     protected override byte[] HashPassword(string plaintext, byte[] salt)
     {
-        throw new NotImplementedException();
+        using var hmac = new HMACSHA512(Convert.FromBase64String(_options.Secret));
+
+        var plaintextBytes = Encoding.UTF8.GetBytes(plaintext);
+        
+        var hashBytes = hmac.ComputeHash(plaintextBytes.Concat(salt).ToArray());
+
+        return hashBytes;
     }
+}
+
+public sealed class PasswordManagerV1Options
+{
+    public required int SaltSize { get; init; }
+    public required string Secret { get; init; }
 }
