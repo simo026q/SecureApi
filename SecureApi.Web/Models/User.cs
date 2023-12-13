9ba9f92ecb50401da2cced3f1e1cf3d1a52a2381
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2023-12-13 22:45:18 +0100

    Added Email to user

--- SecureApi.Web/Models/User.cs
+++ SecureApi.Web/Models/User.cs
@@ -5,4 +5,6 @@ public class User
     public Guid Id { get; init; }
     public required string UserName { get; init; }
     public Password Password { get; set; }
+    public required string Email { get; set; }
+    public bool IsEmailVerified { get; set; }
 }
