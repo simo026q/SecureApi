ffd6473ef39ffcd643b85fad437b0f5cc1ffe6f9
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2023-12-13 22:45:28 +0100

    Added UserDevice

---
+++ SecureApi.Web/Models/UserDevice.cs
@@ -0,0 +1,17 @@
+﻿namespace SecureApi.Web.Models;
+
+public class UserDevice
+{
+    public Guid Id { get; init; }
+    public required string IpAddress { get; init; }
+    public string? UserAgent { get; init; }
+    public string? Language { get; init; }
+    public string? Referrer { get; init; }
+    public string? SecBrand { get; init; }
+    public bool SecIsMobile { get; init; }
+    public string? SecPlatform { get; init; }
+    public string? SecPlatformVersion { get; init; }
+    public string? SecModel { get; init; }
+    public DateTimeOffset LastUsedAt { get; set; }
+    public DateTimeOffset CreatedAt { get; init; }
+}
