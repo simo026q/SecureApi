4ad5e6ab07bcdc27b948fe2f7a599c283235560d
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2023-12-13 21:49:33 +0100

    Added PasswordManagerFactory

---
+++ SecureApi.Web/Services/Cryptography/IPasswordManageFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace SecureApi.Web.Services.Cryptography;
+
+public interface IPasswordManageFactory
+{
+    IPasswordManager Create(int version);
+    IPasswordManager Create();
+}
---
+++ SecureApi.Web/Services/Cryptography/PasswordManagerFactory.cs
@@ -0,0 +1,22 @@
+﻿namespace SecureApi.Web.Services.Cryptography;
+
+public sealed class PasswordManagerFactory
+    : IPasswordManageFactory
+{
+    private readonly IEnumerable<IPasswordManager> _passwordManagers;
+
+    public PasswordManagerFactory(IEnumerable<IPasswordManager> passwordManagers)
+    {
+        _passwordManagers = passwordManagers;
+    }
+
+    public IPasswordManager Create(int version)
+    {
+        return _passwordManagers.Single(x => x.Version == version);
+    }
+
+    public IPasswordManager Create()
+    {
+        return _passwordManagers.MaxBy(x => x.Version)!;
+    }
+}
