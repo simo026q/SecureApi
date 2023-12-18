5eff73620711866efcc424a027afdd80a1f58026
Simon Christensen <31368479+simo026q@users.noreply.github.com>
2023-12-18 21:51:30 +0100

    Added Email struct

---
+++ SecureApi.Web/Models/Email.cs
@@ -0,0 +1,32 @@
+﻿namespace SecureApi.Web.Models;
+
+public readonly struct Email
+    : IEquatable<Email>
+{
+    public string Value { get; }
+    public bool IsVerified { get; }
+
+    public Email(string value, bool isVerified)
+    {
+        Value = value;
+        IsVerified = isVerified;
+    }
+
+    public bool Equals(Email other)
+    {
+        return Value == other.Value && IsVerified == other.IsVerified;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is Email other && Equals(other);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Value, IsVerified);
+    }
+
+    public static bool operator ==(Email left, Email right) => left.Equals(right);
+    public static bool operator !=(Email left, Email right) => !left.Equals(right);
+}
--- SecureApi.Web/Models/User.cs
+++ SecureApi.Web/Models/User.cs
@@ -5,6 +5,5 @@ public class User
     public Guid Id { get; init; }
     public required string UserName { get; init; }
     public Password Password { get; set; }
-    public required string Email { get; set; }
-    public bool IsEmailVerified { get; set; }
+    public Email Email { get; set; }
 }
